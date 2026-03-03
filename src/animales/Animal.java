c5ccfaca59a639ab991aaba9647d808041e4bc48
RaulDaw2026 <465368@alu.murciaeduca.es>
2026-03-03 11:13:22 +0100

    Añadido JavaDoc completo clase Animal

--- src/animales/Animal.java
+++ src/animales/Animal.java
@@ -48,10 +48,24 @@ public abstract class Animal {
         }
     }
 
+ /**
+ * Devuelve el codigo que identifica al animal.
+ *
+ * @return código del animal
+ */
+    
     public String getCodigo() {
         return codigo;
     }
 
+   /**
+ * Modifica el codigo que identifica al animal.
+ *
+ * @param codigo nuevo código (5 caracteres alfanuméricos en minúscula)
+ * @throws IllegalArgumentException si el codigo no cumple el patrón [0-9a-z]{5}
+ */
+    
+    
     public void setCodigo(String codigo) {
         if (!codigo.matches("[0-9a-z]{5}")) {
             throw new IllegalArgumentException();
@@ -59,11 +73,20 @@ public abstract class Animal {
             this.codigo = codigo;
         }
     }
-
+/**
+ * Devuelve la fecha de nacimiento del animal.
+ *
+ * @return fecha de nacimiento
+ */
     public LocalDate getFechaNacimiento() {
         return fechaNacimiento;
     }
-
+/**
+ * Modifica la fecha de nacimiento del animal.
+ *
+ * @param fechaNacimiento fecha en formato ISO-8601 (yyyy-MM-dd)
+ * @throws IllegalArgumentException si la fecha no es valida o no se puede parsear
+ */
     public void setFechaNacimiento(String fechaNacimiento) {
         LocalDate fecha;
 
@@ -75,11 +98,20 @@ public abstract class Animal {
 
         this.fechaNacimiento = fecha;
     }
-
+/**
+ * Devuelve el sexo del animal.
+ *
+ * @return 'M' o 'H'
+ */
     public char getSexo() {
         return sexo;
     }
-
+/**
+ * Modifica el sexo del animal.
+ *
+ * @param sexo 'M' o 'H'
+ * @throws IllegalArgumentException si el sexo no es 'M' ni 'H'
+ */
     public void setSexo(char sexo) {
         if ((sexo != 'M' && sexo != 'H')) {
             throw new IllegalArgumentException();
@@ -87,11 +119,20 @@ public abstract class Animal {
             this.sexo = sexo;
         }
     }
-
+/**
+ * Devuelve el peso del animal.
+ *
+ * @return peso en kilogramos
+ */
     public double getPeso() {
         return peso;
     }
-
+/**
+ * Modifica el peso del animal.
+ *
+ * @param peso nuevo peso en kilogramos, debe ser mayor que 0
+ * @throws IllegalArgumentException si el peso es menor o igual que 0
+ */
     public void setPeso(double peso) {
         if (peso <= 0) {
             throw new IllegalArgumentException();
@@ -99,7 +140,11 @@ public abstract class Animal {
             this.peso = peso;
         }
     }
-
+/**
+ * Calcula el codigo hash del animal en función de sus atributos.
+ *
+ * @return valor hash del objeto
+ */
     @Override
     public int hashCode() {
         int hash = 3;
@@ -109,7 +154,13 @@ public abstract class Animal {
         hash = 19 * hash + (int) (Double.doubleToLongBits(this.peso) ^ (Double.doubleToLongBits(this.peso) >>> 32));
         return hash;
     }
-
+/**
+ * Compara este animal con otro objeto para determinar si son iguales.
+ * Se considera igual si coinciden código, fecha de nacimiento, sexo y peso.
+ *
+ * @param obj objeto a comparar
+ * @return true si son iguales; false en caso contrario
+ */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -136,18 +187,38 @@ public abstract class Animal {
         }
         return true;
     }
-
+/**
+ * Devuelve una representación en texto del animal con sus atributos.
+ *
+ * @return representación textual del animal
+ */
     @Override
     public String toString() {
         return "Animal{" + "codigo=" + codigo + ", fechaNacimiento=" + fechaNacimiento + ", sexo=" + sexo + ", peso=" + peso + '}';
     }
-
+/**
+ * Devuelve el sonido característico del animal.
+ *
+ * @return sonido del animal
+ */
     public abstract String hacerSonido();
-
+/**
+ * Devuelve una cadena que representa la reacción del animal cuando se alegra.
+ *
+ * @return reacción de alegría
+ */
     public abstract String alegrarse();
-
+/**
+ * Devuelve una cadena que representa la reacción del animal cuando se enfada.
+ *
+ * @return reacción de enfado
+ */
     public abstract String enfadarse();
-
+/**
+ * Indica el tipo de animal (ejemplo, "Perro" o "Gato").
+ *
+ * @return tipo o nombre del animal
+ */
     public abstract String queSoy();
 
 }
